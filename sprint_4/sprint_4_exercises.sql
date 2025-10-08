57fea595859ab08fd822b9e652413611190e0459
José <jmessiasgarcia@gmail.com>
2025-10-08 13:45:00 +0200

    Update sprint_4_exercises.sql

--- sprint_4/sprint_4_exercises.sql
+++ sprint_4/sprint_4_exercises.sql
@@ -1,5 +1,4 @@
 USE EuroCommerce;
-
 /*
 Exercise 1 - Sprint 4 - Level 1
 Create a subquery that shows all users with more than 80 transactions using at least 2 tables.
@@ -17,6 +16,16 @@ WHERE (SELECT COUNT(*)
        WHERE t.user_id = du.id) > 80 -- SUBQUERY TO FIND THE NUMBER OF THE TRANSACTION GREATER THAN 80
 ORDER BY number_of_transactions;
 
+SELECT 
+    du.id,
+    CONCAT(du.name, ' ', du.surname) AS full_name
+FROM data_users du
+WHERE (
+    SELECT COUNT(*) 
+    FROM transactions t
+    WHERE t.user_id = du.id) > 80;
+
+
 /*
 Exercise 2 - Sprint 4 - Level 1
 Show the average amount per IBAN of the credit cards for the company Donec Ltd, using at least 2 tables.
@@ -30,45 +39,37 @@ FROM transactions t
 JOIN credit_cards cc on t.card_id = cc.id
 JOIN companies co ON t.business_id = co.id
 WHERE co.company_name = 'Donec Ltd'
-GROUP BY cc.iban, co.company_name;
-
+GROUP BY cc.iban;
 
 /*
 Exercise 1 - Sprint 4 - Level 2
 Create a new table that reflects the status of credit cards based on: if the last three transactions have been declined, 
 then it is inactive; if at least one is not declined, then it is active. Based on this table, answer:
-How many cards are active?
 */
 
-CREATE TABLE card_status AS -- SNAPSHOT
-SELECT 
-    cc.id AS card_id,
+CREATE TABLE card_status AS
+SELECT
+    card_id,
     CASE
-        WHEN COUNT(t3.card_id) = 3
-             AND SUM(CASE WHEN t3.declined = TRUE THEN 1 ELSE 0 END) = 3
-        THEN 'inactive'
+        WHEN SUM(CASE WHEN number_of_row <= 3 AND declined THEN 1 ELSE 0 END) = 3 THEN 'inactive'
         ELSE 'active'
     END AS status_card
-FROM credit_cards cc
-LEFT JOIN LATERAL (
-    SELECT 
-        t.card_id,
-        t.declined
-    FROM transactions t
-    WHERE t.card_id = cc.id
-    ORDER BY t.timestamp DESC, t.id DESC
-    LIMIT 3
-) t3 ON true
-GROUP BY cc.id;
+FROM (
+	SELECT 
+        card_id,
+        declined,
+        ROW_NUMBER() OVER (PARTITION BY card_id ORDER BY timestamp DESC, id DESC) AS number_of_row
+    FROM transactions) t
+GROUP BY card_id;
 
-SELECT * FROM card_status
-WHERE status_card = 'Active';
+/*
+How many cards are active?
+*/
+SELECT * FROM card_status;
 
-SELECT COUNT(card_id) AS active_cars
+SELECT COUNT(*) AS active_cards
 FROM card_status
 WHERE status_card = 'Active';
-
-SHOW TABLES FROM EuroCommerce;
 
 /*
 Exercise 1 - Sprint 4 - Level 3
@@ -91,6 +92,7 @@ ORDER BY p.product_name;
 
 /*
 THE BRIDGE TABLE ALREADY ESTABLISHES THE RELATIONSHIP BETWEEN PRODUCTS AND TRANSACTIONS.
-YOU DON’T NEED TO CREATE ANYTHING ELSE.
 I JUST ADDED THE INFORMATION USING JOINS AND COUNT TO GET THE TOTAL SALES.
 */
+
+
